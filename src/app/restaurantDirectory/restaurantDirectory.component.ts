1932d0f7b46e6e0c247fa2402699ec783b3277f3
Zain Qureshi <zainnqureshii@gmail.com>
2020-07-17 01:06:48 +0500

    added functionality

--- src/app/restaurantDirectory/restaurantDirectory.component.ts
+++ src/app/restaurantDirectory/restaurantDirectory.component.ts
@@ -8,6 +8,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RestaurantDirectory implements OnInit {
 
+  restaurantName: string = '';
+  restaurantCity: string = '';
+  restaurants: any[] = []
+
+  constructor() { }
+
+  onRestaurantAdd() {
+    if(this.restaurantName.trim() !== '' && this.restaurantCity.trim() !== '') {
+      this.restaurants.push({restaurantName: this.restaurantName, restaurantCity: this.restaurantCity})
+      this.restaurantName = '';
+      this.restaurantCity = '';
+    }
+  }
+
   ngOnInit() {
 
   }
